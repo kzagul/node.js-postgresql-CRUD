3bb93efb746540fbe9d7189e42c8dbef118c8a12
Kirill Zagul <zagulkirill@gmail.com>
2022-05-18 21:07:53 +0500

    deploy

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 //packages
 const express = require('express')
-const { Pool } = require('pg')
 const appConfig =  require('./db/AppConfig.js')
 
 require('dotenv').config()
@@ -10,9 +9,10 @@ const cors = require('cors')
 
 //app object
 const app = express()
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(cors())
 
-
-const db = require('./db/DBconnection')
 
 //routes to controllers with API
 const directorRouter = require('./db/routes/director.routes')
@@ -22,11 +22,6 @@ const trainerRouter = require('./db/routes/trainer.routes')
 const sportObjectRouter = require('./db/routes/sportobject.routes')
 const sportInstitutionRouter = require('./db/routes/sportinstitution.routes')
 const userRouter = require('./db/routes/user.routes')
-
-//url links
-app.use(cors())
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json())
 
 app.use('/api', directorRouter)
 app.use('/api', locateRouter)
@@ -42,10 +37,10 @@ app.get('/', async (req, res) => {
     res.send("Heoo")
 })
 
-const PORT = appConfig.Port || 3004
-// const PORT =  3030
+// const PORT = appConfig.Port || 3004
+const PORT =  3030
 
 //checking the server's work
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`listen to port ${PORT}`)
 })
