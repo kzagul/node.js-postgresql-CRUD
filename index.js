1db27eb1674bffb0ea270de06c2020871697705f
Kirill Zagul <zagulkirill@gmail.com>
2022-05-19 16:46:08 +0500

    init

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ require('dotenv').config()
 //cors
 const cors = require('cors')
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 //app object
 const app = express()
 app.use(express.urlencoded({ extended: false }))
@@ -34,6 +42,19 @@ app.get('/', async (req, res) => {
     // res.send("Sport 72 server by KZagul with Node.js and Love")
     res.json({ message: "SPORT node.js application by K.Zagul" });
 })
+
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
 
 
 // const PORT = appConfig.Port || 3004
