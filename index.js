69692cd031c0b34042256904257833d2ef461f3f
Kirill Zagul <zagulkirill@gmail.com>
2022-05-20 00:02:04 +0500

    init

--- index.js
+++ index.js
@@ -43,18 +43,18 @@ app.get('/', async (req, res) => {
     res.json({ message: "SPORT node.js application by K.Zagul" });
 })
 
-// app.get('/db', async (req, res) => {
-//     try {
-//       const client = await pool.connect();
-//       const result = await client.query('SELECT * FROM test_table');
-//       const results = { 'results': (result) ? result.rows : null};
-//       res.render('pages/db', results );
-//       client.release();
-//     } catch (err) {
-//       console.error(err);
-//       res.send("Error " + err);
-//     }
-//   })
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
 
 
 // const PORT = appConfig.Port || 3004
